6205814f6795a113addf3d32b51b88d53d4db552
hp <h.pirhadi@ebcom.ir>
2024-04-22 16:09:18 +0330

    update spark dags

--- src/airflow/dags/compute_session_CTR.py
+++ src/airflow/dags/compute_session_CTR.py
@@ -1,3 +1,27 @@
+# from airflow import DAG 
+# from airflow.providers.ssh.operators.ssh import SSHOperator
+
+# from airflow.operators.bash import BashOperator
+# from airflow.hooks.base import BaseHook
+# from datetime import datetime, timedelta
+
+# default_args = {
+#     'owner': 'airscholar',
+#     'start_date': datetime(2024, 1, 1, 00, 00),
+# }
+
+# # Define Airflow DAG
+# with DAG('sparksql_to_elasticsearch_ssh', 
+#          default_args=default_args, 
+#          schedule_interval='*/10 * * * *', catchup=False) as dag:
+    
+#         task = SSHOperator(
+#                 task_id='submit_job',
+#                 ssh_conn_id='ssh_spark',
+#                 command=f'spark-submit --total-executor-cores 1 --executor-cores 1 --executor-memory 2g --num-executors 1 --driver-memory 2g /opt/spark/apps/pyspark_script.py  --driver-class-path /opt/spark/data/postgresql-42.7.3.jar --jars /opt/spark/data/postgresql-42.7.3.jar'
+#             )
+        
+############### version two ##########################
 from airflow import DAG 
 from airflow.providers.ssh.operators.ssh import SSHOperator
 
@@ -15,8 +39,8 @@ with DAG('sparksql_to_elasticsearch_ssh',
          default_args=default_args, 
          schedule_interval='*/10 * * * *', catchup=False) as dag:
     
-        task = SSHOperator(
-                task_id='submit_job',
-                ssh_conn_id='ssh_spark',
-                command=f'spark-submit --total-executor-cores 1 --executor-cores 1 --executor-memory 2g --num-executors 1 --driver-memory 2g /opt/spark/apps/pyspark_script.py'
-            )
+    task = SSHOperator(
+        task_id='submit_job',
+        ssh_conn_id='ssh_spark',
+        command='sudo python /opt/spark/apps/pyspark_script.py',
+    )
--- src/spark/book_data/pyspark_script.py
+++ src/spark/book_data/pyspark_script.py
@@ -1,37 +1,103 @@
-src/airflow/include/pyspark_script.py# Import necessary libraries
-from pyspark.sql import SparkSession
+# # Import necessary libraries
+# from pyspark.sql import SparkSession
     
-# Define the SparkSQL job
+# # Define the SparkSQL job
+# def spark_sql_job():
+#     # Create SparkSession
+#     spark = SparkSession.builder \
+#         .appName("PostgreSQL to Elasticsearch") \
+#         .config("spark.jars.packages", "org.elasticsearch:elasticsearch-hadoop:7.15.2,org.postgresql:postgresql:42.2.24") \
+#         .getOrCreate()
+
+#     # Read data from PostgreSQL
+#     df = spark.read \
+#         .format("jdbc") \
+#         .option("url", "jdbc:postgresql://postgres_store:5432/advertise") \
+#         .option("dbtable", "ad_interaction") \
+#         .option("user", "test") \
+#         .option("password", "test") \
+#         .load()
+
+#     # Perform SparkSQL query
+#     df.createOrReplaceTempView("temp_table")
+#     result_df = spark.sql("SELECT * FROM temp_table")  # Your SQL query here
+
+#     # Write data to Elasticsearch
+#     # result_df.write \
+#     #     .format("org.elasticsearch.spark.sql") \
+#     #     .option("es.nodes", "elasticsearch") \
+#     #     .option("es.port", "9200") \
+#     #     .option("es.resource", "your_index/your_type") \
+#     #     .save()
+#     print(result_df)
+#     # Stop SparkSession
+#     spark.stop()
+
+# if __name__ == '__main__':
+#     spark_sql_job()
+
+
+#### version two ##################################
+# def spark_sql_job():
+
+#     from pyspark.sql import SparkSession
+
+#     spark = SparkSession.builder\
+#         .appName("PostgreSQL to Elasticsearch")\
+#         .config("spark.jars", "/opt/spark/data/postgresql-42.7.3.jar")\
+#         .config("spark.driver.extraClassPath", "/opt/spark/data/postgresql-42.7.3.jar")\
+#         .getOrCreate()
+
+#     df = spark.read \
+#       .format("jdbc") \
+#       .option("url", "jdbc:postgresql://postgres_store:5432/advertise") \
+#       .option("dbtable", "car_ad") \
+#       .option("user", "test") \
+#       .option("password", "test") \
+#       .load()
+    
+#     df.show()
+
+#     # try:
+#     #     df.write \
+#     #       .format("org.elasticsearch.spark.sql") \
+#     #       .option("es.nodes", "elasticsearch") \
+#     #       .option("es.port", "9200") \
+#     #       .option("es.resource", "car_ad/index") \
+#     #       .save()
+#     # except Exception as e:
+#     #     print(f"Error writing to Elasticsearch: {e}")
+
+#     spark.stop()
+
+# if __name__ == '__main__':
+#     spark_sql_job()
+
+
+
+############## versoin three ################################
 def spark_sql_job():
-    # Create SparkSession
-    spark = SparkSession.builder \
-        .appName("PostgreSQL to Elasticsearch") \
-        .config("spark.jars.packages", "org.elasticsearch:elasticsearch-hadoop:7.15.2,org.postgresql:postgresql:42.2.24") \
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder\
+        .appName("PostgreSQL to Elasticsearch")\
+        .config("spark.jars", "/opt/spark/data/postgresql-42.7.3.jar")\
+        .config("spark.driver.extraClassPath", "/opt/spark/data/postgresql-42.7.3.jar")\
         .getOrCreate()
 
-    # Read data from PostgreSQL
     df = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:postgresql://postgres_store:5432/advertise") \
-        .option("dbtable", "ad_interaction") \
-        .option("user", "test") \
-        .option("password", "test") \
-        .load()
+      .format("jdbc") \
+      .option("url", "jdbc:postgresql://postgres_store:5432/advertise") \
+      .option("dbtable", "car_ad") \
+      .option("user", "test") \
+      .option("password", "test") \
+      .load()
+    
+    df.show()
 
-    # Perform SparkSQL query
-    df.createOrReplaceTempView("temp_table")
-    result_df = spark.sql("SELECT * FROM temp_table")  # Your SQL query here
-
-    # Write data to Elasticsearch
-    # result_df.write \
-    #     .format("org.elasticsearch.spark.sql") \
-    #     .option("es.nodes", "elasticsearch") \
-    #     .option("es.port", "9200") \
-    #     .option("es.resource", "your_index/your_type") \
-    #     .save()
-    print(result_df)
-    # Stop SparkSession
     spark.stop()
 
 if __name__ == '__main__':
     spark_sql_job()
+
+spark = SparkSession.builder.appName("PostgreSQL to Elasticsearch").config("spark.jars", "/opt/spark/data/postgresql-42.7.3.jar").config("spark.driver.extraClassPath", "/opt/spark/data/postgresql-42.7.3.jar").getOrCreate()
